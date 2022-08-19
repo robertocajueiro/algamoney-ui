82784d1272484eab59e13daa164afc02e928950b
robertocajueiro <beto.cajueiro@gmail.com>
2022-08-19 18:06:12 -0300

    Populando base pessoas

--- src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -16,6 +16,10 @@ export class PessoasPesquisaComponent implements OnInit {
     { nome: 'Roberto Dantas', cidade: 'Recife', estado: 'PE', status: 1},
     { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG', status: 1},
     { nome: 'Sebastião Silva', cidade: 'São Paulo', estado: 'SP', status: 0},
+    { nome: 'Carla Souza', cidade: 'Florianopolis', estado: 'SC', status: 0},
+    { nome: 'Luis Pereira', cidade: 'Curitiba', estado: 'PR', status: 0},
+    { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro', estado: 'RJ', status: 0},
+    { nome: 'Lucas Batista', cidade: 'Olinda', estado: 'PE', status: 0},
   ];
 
 }
