c671df00c8e84811bc311c9b0de4fbb16412b72c
robertocajueiro <beto.cajueiro@gmail.com>
2022-10-28 16:56:28 -0300

    Desafio crinado o Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
@@ -20,6 +20,7 @@ import { PessoasModule } from './pessoas/pessoas.module';
 
     LancamentosModule,
     PessoasModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
