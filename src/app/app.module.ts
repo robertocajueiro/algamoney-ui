772c5dcf0f54a6b9a4ecab0ff88460083391b80b
robertocajueiro <beto.cajueiro@gmail.com>
2022-11-14 17:54:07 -0300

    17.10. Adicionando diálogo de confirmação antes da exclusão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 
 import { ToastModule } from 'primeng/toast';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/core.module';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -21,13 +22,15 @@ import { MessageService } from 'primeng/api';
     BrowserAnimationsModule,
 
     ToastModule,
+    ConfirmDialogModule,
 
     LancamentosModule,
     PessoasModule,
     CoreModule
   ],
   providers: [
-    MessageService
+    MessageService,
+    ConfirmationService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { LazyLoadEvent, MessageService } from 'primeng/api';
+import { ConfirmationService, LazyLoadEvent, MessageService } from 'primeng/api';
 import { Table } from 'primeng/table';
 
 import { LancamentoFiltro, LancamentoService } from './../lancamento.service';
@@ -18,7 +18,8 @@ export class LancamentosPesquisaComponent implements OnInit {
 
   constructor(
     private lancamentoService: LancamentoService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
     ){}
 
   ngOnInit(): void{
@@ -41,7 +42,17 @@ export class LancamentosPesquisaComponent implements OnInit {
       this.pesquisar(pagina);
     }
 
+    confirmarExclusao(lancamento: any): void{
+      this.confirmationService.confirm({
+        message: 'Tem certeza que deseja excluir',
+        accept: () => {
+          this.excluir(lancamento);
+        }
+      });
+    }
+
     excluir(lancamento: any){
+
       this.lancamentoService.excluir(lancamento.codigo)
         .then(() =>{
           if(this.grid.first === 0){
