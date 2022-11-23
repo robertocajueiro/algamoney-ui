e5372b5d5b4bf65a1199caf5b4d365af7198a7df
robertocajueiro <beto.cajueiro@gmail.com>
2022-11-23 12:51:16 -0300

    18.2. Configurando rotas na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
+
 import { CoreModule } from './core/core.module';
 import { ConfirmationService, MessageService } from 'primeng/api';
 
 import { TranslateService  } from '@ngx-translate/core';
+
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'pessoas', component: PessoasPesquisaComponent },
+
+];
 
 @NgModule({
   declarations: [
@@ -19,6 +31,7 @@ import { TranslateService  } from '@ngx-translate/core';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
 
     LancamentosModule,
     PessoasModule,
