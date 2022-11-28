cef350030cb0487a13516c3e57089674e4f799d5
robertocajueiro <beto.cajueiro@gmail.com>
2022-11-28 13:48:31 -0300

    19.4. Implementando o serviço de autenticação com OAuth 2

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -7,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  login(usuario: string, senha: string) {  }
+  login(usuario: string, senha: string) {
+    this.auth.login(usuario, senha);
+  }
 
 }
