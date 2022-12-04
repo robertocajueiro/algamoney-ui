372f939acd0dd733eb76300748e3cd9d44f4e7cc
robertocajueiro <beto.cajueiro@gmail.com>
2022-12-04 08:40:56 -0300

    19.11. Interceptando chamadas HTTP para tratar a expiração do access token

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-
 import { FormsModule } from '@angular/forms';
-import {InputTextModule} from 'primeng/inputtext';
 
+
+import {InputTextModule} from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { SegurancaRoutingModule } from './seguranca-routing.module';
 import { LoginFormComponent } from './login-form/login-form.component';
 
 import { JwtModule, JwtHelperService } from '@auth0/angular-jwt';
+import { MoneyHttpInterceptor } from './money-http-interceptor';
 
 export function tokenGetter(): string {
   return localStorage.getItem('token')!;
@@ -35,6 +37,13 @@ export function tokenGetter(): string {
 
     SegurancaRoutingModule
   ],
-  providers: [JwtHelperService]
+  providers: [
+    JwtHelperService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MoneyHttpInterceptor,
+      multi: true
+    }
+  ]
 })
 export class SegurancaModule { }
