42c71d867e88cd42f799b7a33a0a3c16c5cfac16
robertocajueiro <beto.cajueiro@gmail.com>
2022-12-03 22:51:58 -0300

    19.9. Exibindo o menu do sistema conforme permissões do usuário

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -17,4 +17,8 @@ export class NavbarComponent implements OnInit {
     this.usuarioLogado = this.auth.jwtPayload?.nome;
   }
 
+  temPermissao(permissao: string){
+    return this.auth.temPermissao(permissao)
+  }
+
 }
--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -39,6 +39,10 @@ export class AuthService {
       });
   }
 
+  temPermissao(permissao: string){
+    return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
+  }
+
   public armazenarToken(token: string){
     this.jwtPayload = this.jwtHelper.decodeToken(token);
     console.log(this.jwtPayload);
