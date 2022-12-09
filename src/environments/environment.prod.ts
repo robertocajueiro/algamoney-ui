3947995b588e428be5dd6e87094a3906a5c99b96
robertocajueiro <beto.cajueiro@gmail.com>
2022-12-09 10:39:42 -0300

    20.3. Respondendo requisições com Node.js e Express

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoneyui-robertocajueiro.netlify.app',
-  tokenAllowedDomains: [ /algamoneyapi-robertocajueiro.herokuapp.com/ ],
+  apiUrl: 'http://localhost:8080',
+  //apiUrl: 'https://algamoneyui-robertocajueiro.netlify.app',
+  //  tokenAllowedDomains: [ /algamoneyapi-robertocajueiro.herokuapp.com/ ],
+
+  tokenAllowedDomains: [/localhost:8080/],
   tokenDisallowedRoutes: [/\/oauth\/token/],
 };
