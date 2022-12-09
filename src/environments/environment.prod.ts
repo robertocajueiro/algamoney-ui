5b54eb48b9ab55359d71a49f8b305d7f1bb9a4d8
robertocajueiro <beto.cajueiro@gmail.com>
2022-12-09 11:58:15 -0300

    Deploy update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,6 @@ export const environment = {
   apiUrl: 'https://algamoneyapi-robertocajueiro.herokuapp.com',
   tokenAllowedDomains: [ /algamoneyapi-robertocajueiro.herokuapp.com/ ],
   tokenDisallowedRoutes: [/\/oauth\/token/],
-
   //apiUrl: 'http://localhost:8080',
   //tokenAllowedDomains: [/localhost:8080/],
 };
